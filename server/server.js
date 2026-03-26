d766982d4d8badd59d448d7bb67d80c758b2f291
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2026-03-26 22:38:47 +0530

    [Swapnaneel] Add: First Backend Commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { clerkMiddleware } from '@clerk/express'
+
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
+
+app.get("/",(req,res)=>{
+    res.send("server is live");
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server is Running on ${PORT}`);
+})
